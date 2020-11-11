c785f5ee6d2fad14c6b153163068b86b295f0a37
yaoyao <429234595@qq.com>
2020-11-11 11:33:19 +0800

    项目初始化

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 (function (window) {
-	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	const vm = new Vue({
+		el: "#app",
+		data: {
+			todoList: [
+				{id: 1, todo: "打开手机，自拍一张", isCompleted: false},
+				{id: 2, todo: "打开电脑，再拍一张", isCompleted: false},
+				{id: 3, todo: "打开别人的手机，再拍一张", isCompleted: true},
+				{id: 4, todo: "打开PS，修的美美哒", isCompleted: false},
+      ],
+    },
+    
+	});
 
 })(window);
